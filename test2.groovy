9391033da54a103f4f97a952853033325f0ac65a
aneesh898 <83596097+aneesh898@users.noreply.github.com>
2021-05-03 21:12:47 +0530

    Create test2.groovy

---
+++ test2.groovy
@@ -0,0 +1,11 @@
+pipelineJob('pipeline-DSL', {
+    scm {
+        git('git://github.com/quidryan/aws-sdk-test.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}).with closure
