d584eee875b24b3050fe8da29a4870babf5f6df6
aneesh898 <83596097+aneesh898@users.noreply.github.com>
2021-05-20 20:23:52 +0530

    Update dslTest.groovy

--- dslTest.groovy
+++ dslTest.groovy
@@ -5,9 +5,6 @@ job('freestyle-seed-job1') {
 }
 
 pipelineJob('pipeline-seed-job2') {
-     parameters {
-        stringParam('Parameter1', Parameter1, 'Enter a Parameter')
-    }
   definition {
     cps {
       script(readFileFromWorkspace('jenkinsfile'))
